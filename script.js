c321d18bd181dcc2ee750567c20e2b51adb3e2da
Suo Qin <kpsesuo@gmail.com>
2017-07-11 21:19:17 +1000

    list & filters

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ var card = new Vue({
   el: "#card",
   data: {
     title: "Dinosaurs",
-    content: "<strong>Dinosaurs</strong> are a diverse group of animals of the clade <em>Dinosauria</em> that first appeared during the Triassic period."
+    content: "<strong>Dinosaurs</strong> are a diverse group of animals of the clade <em>Dinosauria</em> that first appeared during the Triassic period.",
+    dinos: [
+      {
+        text: "Velociraptor",
+        weight: "15 kg"
+      },
+      {
+        text: "triceratops",
+        weight: "6,000 kg"
+      },
+      {
+        text: "Stegosaurus",
+        weight: "2,500 kg"
+      }
+    ]
+  },
+  filters: {
+    capitalize: (value) => value.charAt(0).toUpperCase() + value.slice(1).toLowerCase(),
+    undercase: (value) => value.toLowerCase(),
+    url: (value) => `https://en.wikipedia.org/wiki/${value}`
   }
 });
