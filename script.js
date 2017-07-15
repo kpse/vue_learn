645a3c8c4ee6664cbfd6714aecd36877ffeb7969
Suo Qin <kpsesuo@gmail.com>
2017-07-16 09:42:59 +1000

    computed properties

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ var card = new Vue({
   data: {
     title: "Dinosaurs",
     content: "<strong>Dinosaurs</strong> are a diverse group of animals of the clade <em>Dinosauria</em> that first appeared during the Triassic period.",
+    dinoUpdated: 0,
+    speciesUpdated: 0,
     dinos: [
       {
         text: "Velociraptor",
-        weight: "15 kg"
+        weight: "15 kg",
+        quantity: 10
       },
       {
         text: "triceratops",
-        weight: "6,000 kg"
+        weight: "6,000 kg",
+        quantity: 8
       },
       {
         text: "Stegosaurus",
-        weight: "2,500 kg"
+        weight: "2,500 kg",
+        quantity: 3
       }
     ]
   },
@@ -24,17 +29,28 @@ var card = new Vue({
     url: (value) => `https://en.wikipedia.org/wiki/${value}`
   },
   methods: {
-    addDino: function() {
+    addDino: function () {
       const input = document.getElementById('dino-form');
-      if(input.value !== '') {
+      if (input.value !== '') {
         this.dinos.push({
-          text: input.value
+          text: input.value,
+          quantity: 0
         });
         input.value = '';
       }
     },
     deleteDino: function (index) {
       this.dinos.splice(index, 1)
+    }
+  },
+  computed: {
+    totalDinos: function () {
+      this.dinoUpdated += 1;
+      return _.sum(_.map(this.dinos, 'quantity'));
+    },
+    totalSpecies: function () {
+      this.speciesUpdated += 1;
+      return this.dinos.length;
     }
   }
 });
